e6720a3d7dc26d25ce2dfdd4327fb914d8a9c907
Steven Kay <stevendkay@gmail.com>
2019-01-29 19:38:51 +0000

    container now calls API and stores student data in state

--- src/containers/hogwarts-container.js
+++ src/containers/hogwarts-container.js
@@ -9,7 +9,8 @@ class HogwartsContainer extends Component {
   constructor(props){
     super(props);
     this.state = {
-      selectedHouse: 0
+      selectedHouse: 0,
+      studentData : []
     };
     this.handleHouseSelected = this.handleHouseSelected.bind(this);
   };
@@ -24,16 +25,37 @@ class HogwartsContainer extends Component {
         />
         <StudentList
           house={this.state.selectedHouse}
+          students={this.state.studentData}
         />
       </div>
     );
   };
 
+  fetchStudentsForSelectedHouse() {
+    const houseName = Houses.names[this.state.selectedHouse];
+    const url = `http://hp-api.herokuapp.com/api/characters/house/${houseName}`;
+    const request = new XMLHttpRequest();
+    request.open('GET', url);
+    request.addEventListener('load', () => {
+      if (request.status === 200) {
+        const jsonData = request.responseText;
+        this.setState({studentData: jsonData});
+        console.dir(jsonData);
+      }
+    });
+    request.send(null);
+  }
+
   handleHouseSelected(event) {
     console.log("Inside handler");
     const selectedHouseNumber = parseInt(event.target.value, 10);
     this.setState({"selectedHouse": selectedHouseNumber});
+    this.fetchStudentsForSelectedHouse();
   };
+
+  componentDidMount() {
+    this.fetchStudentsForSelectedHouse();
+  }
 
 };
 
